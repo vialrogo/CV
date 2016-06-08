c91a09019257c48b174492ac2669c786343b6e22
Victor Romero <vialrogo@gmail.com>
2016-06-08 19:16:44 -0300

    actualization

---
+++ generatePOs.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+po4a-gettextize -f latex -M utf-8 -m cv.pt.tex -p cv.en.po 
+po4a-gettextize -f latex -M utf-8 -m cv.pt.tex -p cv.es.po
---
+++ rebuildSources.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+po4a-translate -f latex -M utf-8 -m cv.pt.tex -p cv.en.po -l cv.en.tex 
+po4a-translate -f latex -M utf-8 -m cv.pt.tex -p cv.es.po -l cv.es.tex
